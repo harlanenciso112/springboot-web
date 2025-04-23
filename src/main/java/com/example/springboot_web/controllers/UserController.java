adeb2b560daa99e1f4c3456af2aa2ed2740bddc2
Harlan Enciso <harsanenciso@gmail.com>
2025-04-22 21:19:26 -0500

    Update UserController.java

--- src/main/java/com/example/springboot_web/controllers/UserController.java
+++ src/main/java/com/example/springboot_web/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
+    //
     @GetMapping("/details")
     public String details(Model model){
         model.addAttribute("title", "Hola mundo");
@@ -15,7 +15,7 @@ public class UserController {
 
         return "details";
     }
-
+    //
     @GetMapping("/info")
     public String info(Model model){
         model.addAttribute("title", "Info");
