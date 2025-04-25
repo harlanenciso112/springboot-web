f80872021bdfff55ee7fe698e3f162723770ba38
Harlan Enciso <harsanenciso@gmail.com>
2025-04-24 20:36:44 -0500

    Update UserController.java

--- src/main/java/com/example/springboot_web/controllers/UserController.java
+++ src/main/java/com/example/springboot_web/controllers/UserController.java
@@ -1,5 +1,5 @@
 package com.example.springboot_web.controllers;
-
+g
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
